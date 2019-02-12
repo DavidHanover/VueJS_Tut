735212380e61226533d2a25edca70c6c5c1ad2b7
Hanover <DavidHanover89@gmail.com>
2019-02-12 11:21:57 -0800

    reformatted vue files with prettier

--- test/src/App.vue
+++ test/src/App.vue
@@ -1,80 +1,77 @@
 <template>
   <div id="app">
-    <Header />
-        <h1>
-            Todos
-        </h1>
-    <AddTodo />
+    <Header/>
+    <h1>Todos</h1>
+    <AddTodo/>
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
-import Header from './components/layout/Header';
-import Todos from './components/Todos.vue';
-import AddTodo from './components/AddTodo';
+import Header from "./components/layout/Header";
+import Todos from "./components/Todos.vue";
+import AddTodo from "./components/AddTodo";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Header,
     Todos,
     AddTodo
-
   },
-  data(){
-    return{
+  data() {
+    return {
       todos: [
         {
-          id:1,
-          title:"Cut grass",
-          completed:false
+          id: 1,
+          title: "Cut grass",
+          completed: false
         },
         {
-          id:2,
-          title:"Go to store",
-          completed:true
+          id: 2,
+          title: "Go to store",
+          completed: true
         },
         {
-          id:3,
-          title:"Get hair-cut",
-          completed:false
+          id: 3,
+          title: "Get hair-cut",
+          completed: false
         },
         {
-          id:4,
-          title:"Clean house",
-          completed:false
+          id: 4,
+          title: "Clean house",
+          completed: false
         }
       ]
-    }
+    };
   },
-  methods:{
-    deleteTodo(id){
+  methods: {
+    deleteTodo(id) {
       this.todos = this.todos.filter(todo => todo.id !== id);
     }
   }
-}
+};
 </script>
 
 <style>
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
-  body {
-    font-family: Arial, Helvetica, sans-serif;
-    line-height: 1.4;
-  }
-  .btn {
-    display: inline-block;
-    border: none;
-    background: #555;
-    color: #fff;
-    padding: 7px 20px;
-    cursor: pointer;
-  }
-  .btn:hover {
-    background: #666;
-  }
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+.btn:hover {
+  background: #666;
+}
 </style>
