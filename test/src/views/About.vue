7330b4b51f5d45ec91e8dcf4daedafa8d784855b
Hanover <DavidHanover89@gmail.com>
2019-02-13 16:55:51 -0800

    some syntax

--- test/src/views/About.vue
+++ test/src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
+    <p>
+      This is the TodoList app v 1.0.0. It was created by David Hanover (
+      <a
+        href="http://davidhanover89.com"
+      >Personal Site</a>) while following the Vue.js tutorial from Brad Traversy that can be found at:
+      <a
+        href="https://www.youtube.com/watch?v=Wy9q22isx3U&t=3034s"
+      >Traversy Vue JS Crash Course - 2019</a>.
+    </p>
   </div>
 </template>
